6676fb09ff2ad8ad9e0e2fd304de7f3e473b112d
Rodrigo Araos <rodrigoaraosq2011@gmail.com>
2022-06-14 14:38:56 -0400

    Se crean las carpetas users e include, donde ahora estarán los TDA. Se genera el gitignore y se crea el main

---
+++ main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <conio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+#include <stdbool.h>
+#include <math.h>
+
+#include "include/list.h"
+#include "include/treemap.h"
+
+int main()
+{
+    return 0;
+}
