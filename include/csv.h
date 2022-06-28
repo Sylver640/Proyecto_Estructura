03fd63540ae63ce0405cd21ae441fd3114361d6c
Rodrigo Araos <rodrigoaraosq2011@gmail.com>
2022-06-28 01:30:01 -0400

    Se eliminan residuos de funciones que se habían eliminado

--- include/csv.h
+++ include/csv.h
@@ -6,7 +6,5 @@ typedef struct movieType movieType;
 char *get_csv_field (char * tmp, int k);
 void split_and_AddGenres(char* genres, movieType* movie);
 int fileExists(char* filename);
-void removeBlankLines(FILE* original, char* path);
-int isBlank(char* str);
 
 #endif
